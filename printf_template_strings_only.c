f455bcb14e453dd3b3d027b1d0361844aead3c51
bandojing <technofrosty@yahoo.com>
2020-03-17 18:07:39 +0000

    commit before checkout

--- printf_template_strings_only.c
+++ printf_template_strings_only.c
@@ -15,22 +15,60 @@ int  _printf(char *format, ...)
 {
 	char *printer;
 	va_list list;
-
 	va_start(list, format);
 
 	while(*format)
 	{
+		if(*format = '\\')
+		{
+		case 'n':
+			putchar(10);
+			format++;
+			continue;
+
 		if(*format == '%')
 		{
 			format++;
 			switch(*format)
 			{
+			case 'x' || 'X':
+				printer = va_arg(list, unsigned int);
+				/*convert to hex*/
+				format++;
+				break;
 			case 's':
 				printer = va_arg(list, char *);
 				_puts(printer);
 				format++;
 				break;
-
+			case 'd':
+				printer = va_arg(list, int);
+				/*function to print integer*/
+				format++;
+				break;
+			case 'i':
+				printer = va_arg(list, int);
+				/*function to print integer*/
+				format++;
+				break;
+			case 'c':
+				printer = va_arg(list, char);
+				putchar(printer);
+				format++;
+				break;
+			case 'p':
+				printer = va_arg(list,);
+				format++;
+				break;
+			case 'u':
+				printer = va_arg(list, long)
+					/*print long here*/
+					format++;
+				break;
+			case 'o':
+				printer = va_arg(list, ?);
+				format++;
+				break;
 			case '\0':
 				break;
 			default:
