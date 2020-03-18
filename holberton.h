e333dd1c1c0d7353fea8f7b7b72b7023ffd095ad
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-18 00:08:59 +0000

    Holberton header file

--- holberton.h
+++ holberton.h
@@ -1,4 +1,11 @@
 #ifndef HOLBERTON_H
-#defin HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdalib.h>
+#include <string.h>
+
+int  _printf(char *format, ...);
 void _puts(char *str);
+
 #endif
